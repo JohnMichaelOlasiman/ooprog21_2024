47ed0868ade977ca66b1be1381a6d311c1d2c636
John Olasiman <121841994+worldofjan@users.noreply.github.com>
2024-08-14 20:18:47 +0800

    Create MovieQuoteInfor.java

---
+++ Chapter 1/MovieQuoteInfor.java
@@ -0,0 +1,15 @@
+public class MovieQuoteInfor {
+    public static void main(String[] args) {
+        
+        String favquote = "Being brave doesn't mean you go looking for trouble.";
+        String movie = "The Lion King";
+        String character = "Mufasa";
+        int year = 1994;
+
+        
+        System.out.println("Quote: \"" + favquote + "\"");
+        System.out.println("Movie: " + movie);
+        System.out.println("Character: " + character);
+        System.out.println("Year: " + year);
+    }
+}
