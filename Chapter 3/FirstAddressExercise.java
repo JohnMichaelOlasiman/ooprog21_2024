cc1b26f3859bd5323e757ca86fff7ff1af4a9f71
John Olasiman <121841994+JohnMichaelOlasiman@users.noreply.github.com>
2024-09-20 21:46:56 +0800

    Create FirstAddressExercise.java

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,12 @@
+public class FirstAddressExercise{
+    public static void main(String[] args){
+        displayAddress();
+    }
+    
+    public static void displayAddress(){
+      System.out.println("Nokia Corporation");
+      System.out.println("Karakaari 7");
+      System.out.println("02610 Espoo, Finland");
+      System.out.println("Company: Nokia");
+    }
+}
